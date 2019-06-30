8920130d541ed6a946c1c140df36e17596bd6b2a
Greenbirch2007 <291109028@qq.com>
2019-06-30 22:25:09 +0800

    豆类跨商品框架数据采集

---
+++ 豆粕-豆油(长期跟踪模型)/国内粕油/M_Y_Spread_China.py
@@ -0,0 +1,109 @@
+
+# ! -*- coding:utf-8 -*-
+# 增加index部位的手数
+import time
+import re
+import pymysql
+from lxml import etree
+import time
+import datetime
+from math import floor
+
+from selenium import webdriver
+
+
+
+# # 2019.6.30 开始统计内盘粕油数据  跑个2年吧
+
+
+
+def call_page(url):
+    driver.get(url)
+    html = driver.page_source
+    return html
+
+# 　选择依次请求５个股票和一个指数后再关闭浏览器
+
+
+
+
+def parse_M_J_Spread():
+    url_M = 'http://quote.eastmoney.com/qihuo/MM.html'
+    html = call_page(url_M)
+    selector = etree.HTML(html)
+    M_Value= selector.xpath('/html/body/div[1]/div[4]/div/div[1]/p[1]/i[1]/text()')
+    for item in M_Value:
+        big_list.append(item)
+
+
+    url_Y = 'http://quote.eastmoney.com/qihuo/ym.html'
+    html = call_page(url_Y)
+    selector = etree.HTML(html)
+    Y_Value= selector.xpath('/html/body/div[1]/div[4]/div/div[1]/p[1]/i[1]/text()')
+    for item in Y_Value:
+        big_list.append(item)
+
+
+
+
+
+
+
+
+
+
+
+
+def insertDB(content):
+    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='Futures',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+    try:
+    # 这里是判断big_list的长度，不是content字符的长度
+        cursor.executemany('insert into M_Y_Spread_China (M_Value,Y_Value,M_Y_Spread) values (%s,%s,%s)', content)
+        connection.commit()
+        connection.close()
+        print('向MySQL中添加数据成功！')
+    except :
+        print('出列啦')
+
+
+
+if __name__ == '__main__':
+
+    while True:
+        driver = webdriver.Chrome()
+
+        big_list = []
+        time.sleep(5)
+
+        parse_M_J_Spread()
+        M = big_list[0]
+        Y = big_list[1]
+        M_Y_Spread = int(M) - int(Y)
+        big_list.append(str(M_Y_Spread))
+
+        driver.quit()
+
+        l_tuple = tuple(big_list)
+        content = []
+        content.append(l_tuple)
+        insertDB(content)
+        print(datetime.datetime.now())
+
+
+#
+
+# M_Value,Y_Value,M_Y_Spread
+
+
+# create table M_Y_Spread_China(
+# id int not null primary key auto_increment,
+# M_Value varchar(10),
+# Y_Value varchar(10),
+# M_Y_Spread varchar(10),
+# LastTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+# ) engine=InnoDB  charset=utf8;
+
+
+# drop  table M221_2A_Index;
---
+++ 豆粕-豆油(长期跟踪模型)/国际豆类3品种框架/A_M_Y_Spread_USA.py
@@ -0,0 +1,121 @@
+
+# ! -*- coding:utf-8 -*-
+# 增加index部位的手数
+import time
+import re
+import pymysql
+from lxml import etree
+import time
+import datetime
+from math import floor
+
+from selenium import webdriver
+
+
+
+# 2019.6.30 开始统计美豆框架数据 跑个2年吧
+
+
+
+def call_page(url):
+    driver.get(url)
+    html = driver.page_source
+    return html
+
+# 　选择依次请求５个股票和一个指数后再关闭浏览器
+
+
+
+
+def parse_A_M_J_Spread():
+    url_A = 'https://finance.sina.com.cn/futures/quotes/S.shtml'
+    html = call_page(url_A)
+    selector = etree.HTML(html)
+    A_Value= selector.xpath('//*[@id="table-box-futures-hq"]/tbody/tr[1]/td[1]/div/span[1]/text()')
+    for item in A_Value:
+        big_list.append(item)
+
+
+    url_M = 'https://finance.sina.com.cn/futures/quotes/SM.shtml'
+    html = call_page(url_M)
+    selector = etree.HTML(html)
+    M_Value= selector.xpath('//*[@id="table-box-futures-hq"]/tbody/tr[1]/td[1]/div/span[1]/text()')
+    for item in M_Value:
+        big_list.append(item)
+
+
+
+    url_Y = 'https://finance.sina.com.cn/futures/quotes/BO.shtml'
+    html = call_page(url_Y)
+    selector = etree.HTML(html)
+    Y_Value= selector.xpath('//*[@id="table-box-futures-hq"]/tbody/tr[1]/td[1]/div/span[1]/text()')
+    for item in Y_Value:
+        big_list.append(item)
+
+
+
+
+
+
+
+
+
+
+
+def insertDB(content):
+    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='Futures',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+    try:
+        cursor.executemany('insert into A_M_Y_Spread_USA (A_value,M_Value,Y_Value,A_Y_Spread,M_Y_Spread) values (%s,%s,%s,%s,%s)', content)
+        connection.commit()
+        connection.close()
+        print('向MySQL中添加数据成功！')
+    except :
+        print('出列啦')
+
+
+
+if __name__ == '__main__':
+
+    while True:
+        driver = webdriver.Chrome()
+
+        big_list = []
+        time.sleep(15)
+
+        parse_A_M_J_Spread()
+        A = big_list[0]
+        M = big_list[1]
+        Y = big_list[2]
+        A_Y_Spread = float(A) - float(Y)
+        M_Y_Spread = float(M) - float(Y)
+        big_list.append(str(A_Y_Spread))
+        big_list.append(str(M_Y_Spread))
+
+        driver.quit()
+
+        l_tuple = tuple(big_list)
+        content = []
+        content.append(l_tuple)
+        insertDB(content)
+        print(datetime.datetime.now())
+
+
+#
+
+# A_value,M_Value,Y_Value,A_Y_Spread,M_Y_Spread
+
+
+# create table A_M_Y_Spread_USA(
+# id int not null primary key auto_increment,
+# A_value varchar(10),
+# M_Value varchar(10),
+# Y_Value varchar(10),
+# A_Y_Spread varchar(10),
+# M_Y_Spread varchar(10),
+# LastTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+# ) engine=InnoDB  charset=utf8;
+
+
+# drop  table M_Y_Spread_USA;
