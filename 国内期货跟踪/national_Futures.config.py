018a021d71068e455b7ef2a927e8c26f38813123
Karson <34601389+Greenbirch2007@users.noreply.github.com>
2022-04-22 17:21:42 +0900

    Create national_Futures.config.py

---
+++ 国内期货跟踪/national_Futures.config.py
@@ -0,0 +1,27 @@
+沪铜主力,cum
+铁矿石主力,im
+沪镍主力,nim
+沪铝主力,alm
+螺纹钢主力,rbm
+沪锌主力,znm
+沪铅主力,pbm
+PTA主力,TAM
+玻璃主力,FGM
+聚氯乙烯主力,vm
+聚丙烯主力,ppm
+聚乙烯主力,lm
+橡胶主力,rum
+焦煤主力,jmm
+焦炭主力,jm
+豆粕主力,mm
+棕榈油主力,pm
+豆油主力,ym
+玉米主力,cm
+白糖主力,SRM
+菜油主力,OIM
+玉米淀粉主力,csm
+一号棉花主力,CFM
+
+http://quote.eastmoney.com/qihuo/CFM.html
+  
+  # //*[@id="app"]/div/div/div[8]/div[1]/div/div[1]/span[1]/span/text()
