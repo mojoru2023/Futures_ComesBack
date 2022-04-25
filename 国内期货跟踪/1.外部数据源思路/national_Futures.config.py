00ab37023aa954bd1d102799ed8c15d9f128e101
Karson <34601389+Greenbirch2007@users.noreply.github.com>
2022-04-25 09:35:01 +0900

    Update national_Futures.config.py

--- 国内期货跟踪/1.外部数据源思路/national_Futures.config.py
+++ 国内期货跟踪/1.外部数据源思路/national_Futures.config.py
@@ -1,9 +1,12 @@
 
 
 
-
 china_futurescode =["ym","vm","TAM","rbm","pm","OIM","mm","MAM","lm""im","FGM""bum","APM"]
+trade_base_params_list=[106.43,125.85,117,70.31,122.95,131.77,36.176,197.95,45.469,101.46,62.533,171.53]
 
+# trade_base_params_dict = dict(zip(china_futurescode,trade_base_params_list))
+trade_base_params_dict = {'ym': 106.43, 'vm': 125.85, 'TAM': 117, 'rbm': 70.31, 'pm': 122.95, 'OIM': 131.77, 'mm': 36.176, 'MAM': 197.95, 'lmim': 45.469, 'FGMbum': 101.46, 'APM': 62.533}
+print(trade_base_params_dict)
 
   
 # 1.东方财富网 https://futures.eastmoney.com/
