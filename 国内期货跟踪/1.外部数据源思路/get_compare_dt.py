1525c99397af4704eeb4fa0c5f245c617462a77c
Karson <34601389+Greenbirch2007@users.noreply.github.com>
2022-04-25 11:44:37 +0900

    Create get_compare_dt.py

---
+++ 国内期货跟踪/1.外部数据源思路/get_compare_dt.py
@@ -0,0 +1,73 @@
+
+
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import datetime
+import pymysql
+import pandas as pd
+import random
+from sqlalchemy import create_engine
+import pymysql
+import pandas as pd
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import datetime
+
+
+
+def fetch_basedt_minus3dt_minus30dt():
+
+    engine_ZN_Futures = create_engine('mysql+pymysql://root:123456@localhost:3306/Futures')
+    # 逆序取数
+    sql_ZN_Futures = 'select * from ZN_Futures   order by id desc ; '
+    # 取第一行数
+    df_ZN_Futures = pd.read_sql_query(sql_ZN_Futures, engine_ZN_Futures)
+    # 取第一行数
+    last_row = list(df_ZN_Futures.iloc[0])
+    # remove id ,remove lasttime
+    base_dt = last_row[1:-1]
+    base_time_string = str(last_row[-1])
+
+
+    #取范围内的3分钟,30分钟的数
+
+    def minum_basetime_minus_N_minutes(string_datetime,N_minutes):
+        string_datetime = datetime.datetime.strptime(string_datetime, "%Y-%m-%d %H:%M:%S")  # 把strTime转化为时间格式,后面的秒位自动补位的
+        minum_basetime_minusN = (string_datetime + datetime.timedelta(minutes=-N_minutes)).strftime("%Y-%m-%d %H:%M:%S")
+        return minum_basetime_minusN
+
+    # minutes 3-4    minutes 29-30
+
+    _base_time_minus3 = minum_basetime_minus_N_minutes(base_time_string,3)
+    _base_time_minus4 = minum_basetime_minus_N_minutes(base_time_string,4)
+    _base_time_minus29 = minum_basetime_minus_N_minutes(base_time_string,29)
+    _base_time_minus30 = minum_basetime_minus_N_minutes(base_time_string,30)
+
+    # 方向是一致的然后选取相同的部分dataframe
+    def difference_set_fromTwo_df(base_df,one_params,df1_time,df2_time):
+        #  df1_time > df2_time
+        df_result = base_df[(base_df[one_params] >= df1_time) & (base_df[one_params] <= df2_time)]
+        result_list = df_result.values.tolist()
+        random_list_result = random.choice(result_list)[1:-1]
+        return random_list_result
+
+    base_dt_minus3_dt = difference_set_fromTwo_df(df_ZN_Futures,"LastTime",_base_time_minus4,_base_time_minus3)
+    base_dt_minus30_dt = difference_set_fromTwo_df(df_ZN_Futures,"LastTime",_base_time_minus30,_base_time_minus29)
+    return base_dt,base_dt_minus3_dt,base_dt_minus30_dt
+
+
+
+
+
+
+base_dt,base_dt_minus3_dt,base_dt_minus30_dt = fetch_basedt_minus3dt_minus30dt()
+print(base_dt,base_dt_minus3_dt,base_dt_minus30_dt)
+
+
+
