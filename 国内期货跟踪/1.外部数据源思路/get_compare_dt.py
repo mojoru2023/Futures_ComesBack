f18790753ed1774379959b00cc89aaa73bfdd922
Greenbirch2007 <min2020fr@gmail.com>
2022-04-25 19:17:36 +0800

    s

--- 国内期货跟踪/1.外部数据源思路/get_compare_dt.py
+++ 国内期货跟踪/1.外部数据源思路/get_compare_dt.py
@@ -5,6 +5,8 @@
 
 import os
 import datetime
+import sys
+
 import pymysql
 import pandas as pd
 import random
@@ -20,54 +22,72 @@ from email.mime.application import MIMEApplication
 import datetime
 
 
+def difference_set_fromTwo_df(base_df, one_params, df1_time, df2_time):
+    #  df1_time > df2_time
+    final_result =None
+    df_result = base_df[(base_df[one_params] >= df1_time) & (base_df[one_params] <= df2_time)]
+    result_list = df_result.values.tolist()
+    random_list_result1 = random.choice(result_list)[1:-1]
+    random_list_result2 = random.choice(result_list)[1:-1]
+    random_list_result3 = random.choice(result_list)[1:-1]
+    if "--" not in random_list_result1:
+        final_result = random_list_result1
+    elif "--" not in random_list_result2:
+        final_result = random_list_result2
+    elif "--" not in random_list_result3:
+        final_result = random_list_result3
+    return final_result
+def minum_basetime_minus_N_minutes(string_datetime, N_minutes):
+    string_datetime = datetime.datetime.strptime(string_datetime, "%Y-%m-%d %H:%M:%S")  # 把strTime转化为时间格式,后面的秒位自动补位的
+    minum_basetime_minusN = (string_datetime + datetime.timedelta(minutes=-N_minutes)).strftime("%Y-%m-%d %H:%M:%S")
+    return minum_basetime_minusN
 
 def fetch_basedt_minus3dt_minus30dt():
 
     engine_ZN_Futures = create_engine('mysql+pymysql://root:123456@localhost:3306/Futures')
     # 逆序取数
-    sql_ZN_Futures = 'select * from ZN_Futures   order by id desc ; '
+    sql_ZN_Futures = 'select * from ZN_Futures  order by id desc ; '
     # 取第一行数
     df_ZN_Futures = pd.read_sql_query(sql_ZN_Futures, engine_ZN_Futures)
     # 取第一行数
     last_row = list(df_ZN_Futures.iloc[0])
     # remove id ,remove lasttime
+    base_id = last_row[0]
     base_dt = last_row[1:-1]
     base_time_string = str(last_row[-1])
-
-
-    #取范围内的3分钟,30分钟的数
-
-    def minum_basetime_minus_N_minutes(string_datetime,N_minutes):
-        string_datetime = datetime.datetime.strptime(string_datetime, "%Y-%m-%d %H:%M:%S")  # 把strTime转化为时间格式,后面的秒位自动补位的
-        minum_basetime_minusN = (string_datetime + datetime.timedelta(minutes=-N_minutes)).strftime("%Y-%m-%d %H:%M:%S")
-        return minum_basetime_minusN
-
-    # minutes 3-4    minutes 29-30
-
+    result_dt = base_dt
     _base_time_minus3 = minum_basetime_minus_N_minutes(base_time_string,3)
     _base_time_minus4 = minum_basetime_minus_N_minutes(base_time_string,4)
     _base_time_minus29 = minum_basetime_minus_N_minutes(base_time_string,29)
     _base_time_minus30 = minum_basetime_minus_N_minutes(base_time_string,30)
-
-    # 方向是一致的然后选取相同的部分dataframe
-    def difference_set_fromTwo_df(base_df,one_params,df1_time,df2_time):
-        #  df1_time > df2_time
-        df_result = base_df[(base_df[one_params] >= df1_time) & (base_df[one_params] <= df2_time)]
-        result_list = df_result.values.tolist()
-        random_list_result1 = random.choice(result_list)[1:-1]
-        random_list_result2 = random.choice(result_list)[1:-1]
-        random_list_result3 = random.choice(result_list)[1:-1]
-        if "--" not in random_list_result1:
-            final_result = random_list_result1
-        elif "--" not in random_list_result2:
-            final_result = random_list_result2
-        elif "--" not in random_list_result3:
-            final_result = random_list_result3
-        return final_result
-
     base_dt_minus3_dt = difference_set_fromTwo_df(df_ZN_Futures,"LastTime",_base_time_minus4,_base_time_minus3)
     base_dt_minus30_dt = difference_set_fromTwo_df(df_ZN_Futures,"LastTime",_base_time_minus30,_base_time_minus29)
-    return base_dt,base_dt_minus3_dt,base_dt_minus30_dt
+
+    if "--" in base_dt:
+        engine_ZN_Futures2 = create_engine('mysql+pymysql://root:123456@localhost:3306/Futures')
+        # 逆序取数
+        sql_ZN_Futures2 = 'select * from ZN_Futures where id ={0} ; '.format(base_id-1)
+        # 取第一行数
+        df_ZN_Futures2 = pd.read_sql_query(sql_ZN_Futures2, engine_ZN_Futures2)
+        # 取第一行数
+        last_row2 = list(df_ZN_Futures2.iloc[0])
+        base_dt2 = last_row2[1:-1]
+        result_dt = base_dt2
+
+
+    #取范围内的3分钟,30分钟的数
+
+
+
+    # minutes 3-4    minutes 29-30
+
+
+
+    # 方向是一致的然后选取相同的部分dataframe
+
+
+
+    return result_dt,base_dt_minus3_dt,base_dt_minus30_dt
 
 
 
--- 国内期货跟踪/1.外部数据源思路/national_Futures.config.py
+++ 国内期货跟踪/1.外部数据源思路/national_Futures.config.py
@@ -1,13 +1,26 @@
 
 
 
-china_futurescode =["ym","vm","TAM","rbm","pm","OIM","mm","MAM","lm""im","FGM""bum","APM"]
-trade_base_params_list=[106.43,125.85,117,70.31,122.95,131.77,36.176,197.95,45.469,101.46,62.533,171.53]
+china_futurescode =["ym","vm","TAM","rbm","pm","OIM","mm","MAM","lm","im","FGM","bum","APM"]
+trade_base_params_list=[106.43,125.85,117,70.31,122.95,131.77,36.1,31.176,197.95,45.46,101.4,62.533,117.53]
 
-# trade_base_params_dict = dict(zip(china_futurescode,trade_base_params_list))
-trade_base_params_dict = {'ym': 106.43, 'vm': 125.85, 'TAM': 117, 'rbm': 70.31, 'pm': 122.95, 'OIM': 131.77, 'mm': 36.176, 'MAM': 197.95, 'lmim': 45.469, 'FGMbum': 101.46, 'APM': 62.533}
+trade_base_params_dict={}
+trade_base_params_dict["ym"] =106.43
+trade_base_params_dict["vm"] =125.85
+trade_base_params_dict["TAM"] =117
+trade_base_params_dict["rbm"] =70.31
+trade_base_params_dict["pm"] =122.95
+trade_base_params_dict["OIM"] =131.77
+trade_base_params_dict["mm"] =36.1
+trade_base_params_dict["MAM"] =31.176
+trade_base_params_dict["lm"] =197.95
+trade_base_params_dict["im"] =45.46
+trade_base_params_dict["FGM"] =101.4
+trade_base_params_dict["bum"] =62.533
+trade_base_params_dict["APM"] =117.53
+
 print(trade_base_params_dict)
-
+# {'ym': 106.43, 'vm': 125.85, 'TAM': 117, 'rbm': 70.31, 'pm': 122.95, 'OIM': 131.77, 'mm': 36.1, 'MAM': 31.176, 'lm': 197.95, 'im': 45.46, 'FGM': 101.4, 'bum': 62.533, 'APM': 117.53}
   
 # 1.东方财富网 https://futures.eastmoney.com/
 
