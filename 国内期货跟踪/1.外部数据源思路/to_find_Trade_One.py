ab77c508cfef9c107132a6351d215a43f134afcf
Karson <34601389+Greenbirch2007@users.noreply.github.com>
2022-04-26 17:56:19 +0900

    Update to_find_Trade_One.py

--- 国内期货跟踪/1.外部数据源思路/to_find_Trade_One.py
+++ 国内期货跟踪/1.外部数据源思路/to_find_Trade_One.py
@@ -1,6 +1,14 @@
 
 
 
+import os
+import sys
+import time
+import datetime
+
+import subprocess
+
+
 
 
 
@@ -16,7 +24,77 @@ import csv
 # 信号确认 有信号,无信号
 # 有信号时创建文件,退出
 # 下次进场,有文件,读取文件信息,结合当次数据发送信号确认的情况。
-trade_dict ={}
+import os
+import datetime
+import sys
+
+import pymysql
+import pandas as pd
+import random
+from sqlalchemy import create_engine
+import pymysql
+import pandas as pd
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import datetime
+
+
+def difference_set_fromTwo_df(base_df, one_params, df1_time, df2_time):
+    #  df1_time > df2_time
+    final_result =None
+    df_result = base_df[(base_df[one_params] >= df1_time) & (base_df[one_params] <= df2_time)]
+    result_list = df_result.values.tolist()
+    random_list_result1 = random.choice(result_list)[1:-1]
+    random_list_result2 = random.choice(result_list)[1:-1]
+    random_list_result3 = random.choice(result_list)[1:-1]
+    if "--" not in random_list_result1:
+        final_result = random_list_result1
+    elif "--" not in random_list_result2:
+        final_result = random_list_result2
+    elif "--" not in random_list_result3:
+        final_result = random_list_result3
+    return final_result
+def minum_basetime_minus_N_minutes(string_datetime, N_minutes):
+    string_datetime = datetime.datetime.strptime(string_datetime, "%Y-%m-%d %H:%M:%S")  # 把strTime转化为时间格式,后面的秒位自动补位的
+    minum_basetime_minusN = (string_datetime + datetime.timedelta(minutes=-N_minutes)).strftime("%Y-%m-%d %H:%M:%S")
+    return minum_basetime_minusN
+
+def fetch_basedt_minus3dt_minus30dt():
+
+    engine_ZN_Futures = create_engine('mysql+pymysql://root:123456@localhost:3306/Futures')
+    # 逆序取数
+    sql_ZN_Futures = 'select * from ZN_Futures  order by id desc ; '
+    # 取第一行数
+    df_ZN_Futures = pd.read_sql_query(sql_ZN_Futures, engine_ZN_Futures)
+    # 取第一行数
+    last_row = list(df_ZN_Futures.iloc[0])
+    # remove id ,remove lasttime
+    base_id = last_row[0]
+    base_dt = last_row[1:-1]
+    base_time_string = str(last_row[-1])
+    result_dt = base_dt
+    _base_time_minus3 = minum_basetime_minus_N_minutes(base_time_string,3)
+    _base_time_minus4 = minum_basetime_minus_N_minutes(base_time_string,4)
+    _base_time_minus29 = minum_basetime_minus_N_minutes(base_time_string,29)
+    _base_time_minus30 = minum_basetime_minus_N_minutes(base_time_string,30)
+    base_dt_minus3_dt = difference_set_fromTwo_df(df_ZN_Futures,"LastTime",_base_time_minus4,_base_time_minus3)
+    base_dt_minus30_dt = difference_set_fromTwo_df(df_ZN_Futures,"LastTime",_base_time_minus30,_base_time_minus29)
+
+    if "--" in base_dt:
+        engine_ZN_Futures2 = create_engine('mysql+pymysql://root:123456@localhost:3306/Futures')
+        # 逆序取数
+        sql_ZN_Futures2 = 'select * from ZN_Futures where id ={0} ; '.format(base_id-1)
+        # 取第一行数
+        df_ZN_Futures2 = pd.read_sql_query(sql_ZN_Futures2, engine_ZN_Futures2)
+        # 取第一行数
+        last_row2 = list(df_ZN_Futures2.iloc[0])
+        base_dt2 = last_row2[1:-1]
+        result_dt = base_dt2
+    return result_dt,base_dt_minus3_dt,base_dt_minus30_dt
 
 
 
@@ -58,7 +136,7 @@ def remove_file(filetype):
         if len(file_list) != 1:
             if file.split(".")[1] == filetype:
                 os.remove(file)
-def find_and_confirm_signal(tradeone,base_dt,base_dt_minus3,base_dt_minus30):
+def find_and_confirm_signal(tradeone,base_dt,base_dt_minus3,base_dt_minus30,trade_dict):
     # 识别信号
     if os.path.exists('call{0}.txt'.format(tradeone)) is False and os.path.exists('put{0}.txt'.format(tradeone)) is False:
         # 无文件,进行信号甄别;有文件,就验证信号
@@ -169,7 +247,71 @@ def find_and_confirm_signal(tradeone,base_dt,base_dt_minus3,base_dt_minus30):
                     remove_existfile("put{0}.txt")
 
 
+
+def use_subprocess_command(command_string):
+
+
+    process = subprocess.Popen(command_string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process.wait()
+    command_output = process.stdout.read().decode('utf-8')
+    # command_output:str
+    return command_output
+
+
+
 if __name__=="__main__":
-    # 开始时, 删除所有文本
-    remove_file("txt")
+    trade_base_params_dict = {'ym': 106.43, 'vm': 125.85, 'TAM': 117, 'rbm': 70.31, 'pm': 122.95, 'OIM': 131.77,
+                              'mm': 36.1, 'MAM': 31.176, 'lm': 197.95, 'im': 45.46, 'FGM': 101.4, 'bum': 62.533,
+                              'APM': 117.53}
+    while True:
+
+        use_subprocess_command("python3 Daily_fetch_dt_DF.py")
+        time.sleep(0.1)
+        use_subprocess_command("python3 Daily_fetch_dt_from_sina.py")
+        time.sleep(0.1)
+        use_subprocess_command("python3 Daily_fetch_dt_SecuS.py ")
+        time.sleep(0.1)
+        use_subprocess_command("python3 Daily_fetch_dt_YC.py")
+        time.sleep(0.1)
+        e = datetime.datetime.now()
+        base_dt, base_dt_minus3_dt, base_dt_minus30_dt = fetch_basedt_minus3dt_minus30dt()
+        # 开始时, 删除所有文本
+        remove_file("txt")
+        find_and_confirm_signal("ym",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("vm",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("TAM",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("rbm",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("pm",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("OIM",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("mm",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("MAM",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("lm",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("im",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("FGM",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("bum",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+        find_and_confirm_signal("APM",base_dt,base_dt_minus3_dt,base_dt_minus30_dt,trade_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
